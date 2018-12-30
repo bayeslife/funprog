81ce14946adba4a75940a3433ff6c81a050b84e2
Phil Tomlinson <ptomlinson@quartileone.com>
2018-12-30 17:42:53 +1000

    Building umd package now

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import resolve    from 'rollup-plugin-node-resolve';
+import commonjs   from 'rollup-plugin-commonjs';
+import pkg        from './package.json';
+
+const { version, author, name, main, license, description } = pkg;
+
+const banner = `\
+/**
+ * ${name} v${version}
+ * ${description}
+ *
+ * @author ${author}
+ * @license ${license}
+ * @preserve
+ */
+`;
+
+// var jsonconfig={
+//   // All JSON files will be parsed by default,
+//   // but you can also specifically include/exclude files
+//   include: 'src/**',
+//   exclude: [ ],
+
+//   // for tree-shaking, properties will be declared as
+//   // variables, using either `var` or `const`
+//   preferConst: true, // Default: false
+
+//   // specify indentation for the generated default export —
+//   // defaults to '\t'
+//   indent: '  '
+// }
+
+export default [
+  {
+    input: 'index.js',
+    output: {
+      file: "dist/lib.umd.js",
+      name: "library",
+      sourcemap: true,
+      format: 'umd',
+      banner
+    },
+    plugins: [
+      resolve(),  // so Rollup can find external libs
+      commonjs(), // so Rollup can convert commonJS to an ES module
+    ]
+  }
+]
