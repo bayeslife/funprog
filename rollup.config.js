ccb495ebbb31b3b6c4924fc4ffc7ba170b578f54
Phil Tomlinson <ptomlinson@quartileone.com>
2018-12-30 19:12:15 +1000

    Another attempt

--- rollup.config.js
+++ rollup.config.js
@@ -1,63 +1,30 @@
-import resolve    from 'rollup-plugin-node-resolve';
-import commonjs   from 'rollup-plugin-commonjs';
-import pkg        from './package.json';
+import { uglify } from 'rollup-plugin-uglify'
 
-const { version, author, name, main, license, description } = pkg;
+const input = 'index.js'
+const sourcemap = true
 
-const banner = `\
-/**
- * ${name} v${version}
- * ${description}
- *
- * @author ${author}
- * @license ${license}
- * @preserve
- */
-`;
-
-// var jsonconfig={
-//   // All JSON files will be parsed by default,
-//   // but you can also specifically include/exclude files
-//   include: 'src/**',
-//   exclude: [ ],
-
-//   // for tree-shaking, properties will be declared as
-//   // variables, using either `var` or `const`
-//   preferConst: true, // Default: false
-
-//   // specify indentation for the generated default export —
-//   // defaults to '\t'
-//   indent: '  '
-// }
-
-export default [
-  {
-    input: 'index.js',
+export default [{
+    input,
     output: {
-      file: "dist/lib.umd.js",
-      name: "funprog",
-      sourcemap: true,
-      format: 'umd',
-      banner
-    },
-    plugins: [
-      resolve(),  // so Rollup can find external libs
-      commonjs(), // so Rollup can convert commonJS to an ES module
-    ]
-  },
-  // {
-  //   input: 'index.js',
-  //   output: {
-  //     file: "dist/lib.esm.js",
-  //     name: 'lib',
-  //     sourcemap: true,
-  //     format: 'esm',
-  //     banner
-  //   },
-  //   plugins: [
-  //     resolve(),
-  //     //commonjs(),
-  //     //json(jsonconfig)
-  //   ]
-  // }
-]
+        file: 'dist/funprog.mjs',
+        format: 'es',
+        sourcemap
+    }
+}, {
+    input,
+    output: {
+        file: 'dist/funprog.js',
+        format: 'cjs',
+        sourcemap
+    }
+},
+{
+    input,
+    output: {
+        file: 'dist/funprog.min.js',
+        format: 'umd',
+        name: 'ss',
+        sourcemap,
+        plugins: [uglify()]
+    }
+}]
